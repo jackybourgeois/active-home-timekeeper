29b5c765481430df23fa2e48e88e03df147843f2
Jacky Bourgeois <jacky.bourgeois@gmail.com>
2016-12-12 14:04:44 +0100

    upgrade kevoree version and cleaning

--- src/main/java/org/activehome/timekeeper/Timekeeper.java
+++ src/main/java/org/activehome/timekeeper/Timekeeper.java
@@ -24,7 +24,6 @@ package org.activehome.timekeeper;
  * #L%
  */
 
-
 import com.eclipsesource.json.JsonObject;
 import org.activehome.com.Notif;
 import org.activehome.com.Request;
@@ -36,7 +35,6 @@ import org.activehome.service.RequestHandler;
 import org.activehome.time.Tic;
 import org.activehome.time.TimeCommand;
 import org.activehome.time.TimeStatus;
-import org.activehome.tools.SunsetSunrise;
 import org.kevoree.annotation.ComponentType;
 import org.kevoree.annotation.KevoreeInject;
 import org.kevoree.annotation.Output;
@@ -55,55 +53,139 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * Manage the time for sync of distributed components and simulation.
+ *
  * @author Jacky Bourgeois
- * @version %I%, %G%
  */
-@ComponentType(description = "Manage the time for sync of distributed components and simulation.")
+@ComponentType(version = 1, description = "Manage the time for sync of "
+        + "distributed components and simulation.")
 public class Timekeeper extends Service implements ModelListener {
 
+    /**
+     * Where to find the sources (for the Active Home store).
+     */
     @Param(defaultValue = "/active-home-timekeeper")
     private String src;
-
-    private TimeStatus status;
-    private TimeZone timezone;
-    private long startTS;
-    private long initTS;
-    private long idleDuration;
-    private long pauseTS;
-    private int zip;
-    private long ticFrequency;
-    private SimpleDateFormat df;
-
-    private Boolean dayTime;
-
-    private ScheduledThreadPoolExecutor stpe;
-
     /**
      * The start time of the system.
      * By default, use the actual/current time.
      */
     @Param(defaultValue = "actual")
     private String startDate;
+    /**
+     * Factor of time compression.
+     * x1 (default) means actual speed
+     */
     @Param(defaultValue = "x1")
     private String zipFactor;
+    /**
+     * Timezone of the timekeeper.
+     */
     @Param(defaultValue = "Europe/London")
     private String timezoneName;
+    /**
+     * Latitude of the timekeeper (to retrieve geographical
+     * info such as sunrise and sunset).
+     */
+    @Param(defaultValue = "52.041404")
+    private double latitude;
+    /**
+     * Longitude of the timekeeper (to retrieve geographical
+     * info such as sunrise and sunset).
+     */
+    @Param(defaultValue = "-0.72878")
+    private double longitude;
+    /**
+     * Generate a log for each tic.
+     */
     @Param(defaultValue = "false")
     private boolean showTic;
 
+    /**
+     * Port to push the {@code Tic}.
+     */
     @Output
-    private
-    org.kevoree.api.Port tic;
-
+    private org.kevoree.api.Port tic;
+    /**
+     * Access to Kevoree's model.
+     */
     @KevoreeInject
     private ModelService modelService;
 
+    /**
+     * Keep the current {@link TimeStatus}.
+     */
+    private TimeStatus status = TimeStatus.UNKNOWN;
+    /**
+     * Time zone of the system.
+     */
+    private TimeZone timezone;
+    /**
+     * Time initialized when calling {@link #init}.
+     */
+    private long startTS = -1;
+    /**
+     * Actual system time used to compute the delta
+     * with Active Home time (same as start time if actual time).
+     */
+    private long initTS;
+    /**
+     * Time spent on pause to add to time delta in simulation mode.
+     * (Combining all idle time since start time)
+     */
+    private long idleDuration = 0;
+    /**
+     * When the current pause started.
+     */
+    private long pauseTS;
+    /**
+     * The time compression factor as number.
+     */
+    private int zip = 1;
+    /**
+     * The tic frequency as number.
+     */
+    private long ticFrequency = HOUR;
+    /**
+     * Date/time format used for time parameters.
+     */
+    private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    /**
+     * Is it currently day time?
+     */
+    private Boolean dayTime;
+    /**
+     * Scheduler used to send Tics.
+     */
+    private ScheduledThreadPoolExecutor stpe;
+
     @Override
     protected final RequestHandler getRequestHandler(final Request request) {
-        return new TimekeeperRequestHandler(request, this);
+        return new TimekeeperRequestHandler(this);
     }
 
-    public final boolean init() {
+    /**
+     * Start the component, reading Kevoree params to set the
+     * timezone, start time, zip, and tic frequency.
+     */
+    @Start
+    public final void start() {
+        super.start();
+        modelService.registerModelListener(this);
+        df.setTimeZone(TimeZone.getTimeZone("UTC"));
+        timezone = TimeZone.getTimeZone(timezoneName);
+        setStartTime(startDate);
+        setZip(zipFactor);
+        setTicFrequency();
+    }
+
+    /**
+     * Switch the status to INITIALIZED and send a Tic
+     * which contains the time command INIT.
+     *
+     * @return true
+     */
+    final boolean init() {
         status = TimeStatus.INITIALIZED;
         initExecutor();
         sendTic(new Tic(getUTCTime(), zip, getTimezoneOffset(),
@@ -114,34 +196,13 @@ public class Timekeeper extends Service implements ModelListener {
         return true;
     }
 
-    @Start
-    public final void start() {
-        super.start();
-        modelService.registerModelListener(this);
-        df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        df.setTimeZone(TimeZone.getTimeZone("UTC"));
-        idleDuration = 0;
-        timezone = TimeZone.getTimeZone(timezoneName);
-        if (startDate.compareTo("actual") != 0) {
-            try {
-                long localStartTime = df.parse(startDate).getTime();
-                startTS = localStartTime - timezone.getOffset(localStartTime);
-                zip = Integer.valueOf(zipFactor.replace("x", ""));
-                ticFrequency = HOUR / zip;
-            } catch (Exception e) {
-                startTS = System.currentTimeMillis();
-                zip = 1;
-                ticFrequency = HOUR;
-            }
-            status = TimeStatus.UNKNOWN;
-        } else {
-            startTS = -1;
-            zip = 1;
-            ticFrequency = HOUR;
-        }
-    }
-
-    public final boolean startTime() {
+    /**
+     * Start sending Tic at the given time zip and frequency.
+     * The first Tic contains the time command START.
+     *
+     * @return true if the time was initialize before
+     */
+    final boolean startTime() {
         if (status == TimeStatus.INITIALIZED) {
             initTS = System.currentTimeMillis();
             idleDuration = 0;
@@ -156,49 +217,13 @@ public class Timekeeper extends Service implements ModelListener {
         return false;
     }
 
-    public final void sendTic(final Tic ticToSend) {
-        if (tic != null && tic.getConnectedBindingsSize() > 0) {
-            tic.send(ticToSend.toString(), null);
-        }
-    }
-
-    public final void tic() {
-        if (showTic) {
-            logInfo("Tic");
-        }
-        if (tic != null && tic.getConnectedBindingsSize() > 0) {
-            Tic nextTic = new Tic(getUTCTime(), zip,
-                    getTimezoneOffset(), status, TimeCommand.CARRYON);
-            tic.send(nextTic.toString(), null);
-        }
-    }
-
-    public final void checkSunsetSunrise() {
-        SunsetSunrise sunsetSunrise = new SunsetSunrise(52.041404, -0.72878,
-                new Date(getUTCTime()), getTimezoneOffset());
-        if (dayTime == null || !dayTime.equals(sunsetSunrise.isDaytime())) {
-            dayTime = sunsetSunrise.isDaytime();
-            DataPoint dpDayTime = new DataPoint("time.dayTime", getUTCTime(),
-                    dayTime + "");
-            sendNotif(new Notif(getFullId(), getNode() + ".context",
-                    getUTCTime(), dpDayTime));
-        }
-
-        long nextCheck;
-        if (getUTCTime() < sunsetSunrise.getSunrise().getTime()) {
-            nextCheck = (sunsetSunrise.getSunrise().getTime() - getUTCTime());
-        } else if (dayTime) {
-            nextCheck = (sunsetSunrise.getSunset().getTime() - getUTCTime());
-        } else {
-            SunsetSunrise nextSS = new SunsetSunrise(52.041404, -0.72878,
-                    new Date(getUTCTime() + DAY), getTimezoneOffset());
-            nextCheck = (nextSS.getSunrise().getTime() - getUTCTime());
-        }
-        stpe.schedule(this::checkSunsetSunrise,
-                nextCheck / zip, TimeUnit.MILLISECONDS);
-    }
-
-    public final boolean pauseTime() {
+    /**
+     * Stop sending regular Tics with a last one containing
+     * the time command PAUSE.
+     *
+     * @return true if the time was running
+     */
+    final boolean pauseTime() {
         if (startTS != -1) {
             if (status == TimeStatus.RUNNING) {
                 logInfo("Pausing time.");
@@ -215,7 +240,13 @@ public class Timekeeper extends Service implements ModelListener {
         return false;
     }
 
-    public final boolean resumeTime() {
+    /**
+     * Restart sending regular Tics with the first one containing
+     * the time command RESUME.
+     *
+     * @return true if the time was idled
+     */
+    final boolean resumeTime() {
         if (startTS != -1) {
             if (status == TimeStatus.IDLE) {
                 logInfo("Resuming time.");
@@ -235,7 +266,13 @@ public class Timekeeper extends Service implements ModelListener {
         return false;
     }
 
-    public final boolean stopTime() {
+    /**
+     * Stop sending regular Tics with a last one containing
+     * the time command STOP.
+     *
+     * @return true if time was running or idled
+     */
+    final boolean stopTime() {
         if (startTS != -1) {
             if (status == TimeStatus.RUNNING || status == TimeStatus.IDLE) {
                 logInfo("Stopping time.");
@@ -252,11 +289,71 @@ public class Timekeeper extends Service implements ModelListener {
         return false;
     }
 
+    /**
+     * Send Tic through tic port.
+     *
+     * @param aTic to send
+     */
+    private void sendTic(final Tic aTic) {
+        if (tic != null && tic.getConnectedBindingsSize() > 0) {
+            tic.send(aTic.toString(), null);
+        }
+    }
+
+    /**
+     *
+     */
+    private void tic() {
+        if (showTic) {
+            logInfo("Tic");
+        }
+        if (tic != null && tic.getConnectedBindingsSize() > 0) {
+            Tic nextTic = new Tic(getUTCTime(), zip,
+                    getTimezoneOffset(), status, TimeCommand.CARRYON);
+            tic.send(nextTic.toString(), null);
+        }
+    }
+
+    /**
+     *
+     */
+    private void checkSunsetSunrise() {
+        SunsetSunrise sunsetSunrise = new SunsetSunrise(latitude, longitude,
+                new Date(getUTCTime()), getTimezoneOffset());
+        if (dayTime == null || !dayTime.equals(sunsetSunrise.isDaytime())) {
+            dayTime = sunsetSunrise.isDaytime();
+            DataPoint dpDayTime = new DataPoint("time.dayTime", getUTCTime(),
+                    dayTime + "");
+            sendNotif(new Notif(getFullId(), getNode() + ".context",
+                    getUTCTime(), dpDayTime));
+        }
+
+        long nextCheck;
+        if (getUTCTime() < sunsetSunrise.getSunrise().getTime()) {
+            nextCheck = (sunsetSunrise.getSunrise().getTime() - getUTCTime());
+        } else if (dayTime) {
+            nextCheck = (sunsetSunrise.getSunset().getTime() - getUTCTime());
+        } else {
+            SunsetSunrise nextSS = new SunsetSunrise(latitude, longitude,
+                    new Date(getUTCTime() + DAY), getTimezoneOffset());
+            nextCheck = (nextSS.getSunrise().getTime() - getUTCTime());
+        }
+        stpe.schedule(this::checkSunsetSunrise,
+                nextCheck / zip, TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * Get the current UTC time: emulated idle/running or actual.
+     *
+     * @return UNIX timestamp.
+     */
     public final long getUTCTime() {
         if (startTS != -1) {
             if (status == TimeStatus.RUNNING) {
-                return startTS + (System.currentTimeMillis() - initTS - idleDuration) * zip;
-            } else if (status == TimeStatus.IDLE || status == TimeStatus.STOPPED) {
+                return startTS + (System.currentTimeMillis()
+                        - initTS - idleDuration) * zip;
+            } else if (status == TimeStatus.IDLE
+                    || status == TimeStatus.STOPPED) {
                 return startTS + (pauseTS - initTS - idleDuration) * zip;
             }
             return startTS;
@@ -264,49 +361,98 @@ public class Timekeeper extends Service implements ModelListener {
         return System.currentTimeMillis();
     }
 
-    public final int getTimezoneOffset() {
+    /**
+     * @return the number of hours to shift in order
+     * to get the time zone from UTC time.
+     */
+    private int getTimezoneOffset() {
         return (int) (timezone.getOffset(getUTCTime()) / HOUR);
     }
 
-    public final Object setProperties(final JsonObject properties) {
+    /**
+     * Set new time properties and call {@code #init}.
+     *
+     * @param properties The new properties to set up.
+     * @return {@code #getProperties} showing the new status of all properties.
+     */
+    public final JsonObject setProperties(final JsonObject properties) {
         if (properties.get("timezone") != null) {
-            timezone = TimeZone.getTimeZone(properties.get("timezone").asString());
+            timezone = TimeZone.getTimeZone(
+                    properties.get("timezone").asString());
         }
 
-        try {
-            startTS = 0;
-            if (properties.get("startDate") != null) {
-                if (properties.get("startDate").asString().compareTo("actual") == 0) {
-                    startTS = System.currentTimeMillis();
-                } else {
-                    long localStart = df.parse(properties.get("startDate").asString()).getTime();
-                    startTS = localStart - timezone.getOffset(localStart);
-                }
-            } else if (properties.get("startTS") != null) {
-                startTS = properties.get("startTS").asLong();
+        if (properties.get("start") != null) {
+            if (properties.get("start").isString()) {
+                setStartTime(properties.get("start").asString());
+            } else if (properties.get("start").isNumber()) {
+                startTS = properties.get("start").asLong();
             }
-
-            int newZip = 1;
-            if (properties.get("zip") != null) {
-                newZip = properties.get("zip").asInt();
-                if (newZip == 0) {
-                    newZip = 1;
-                }
-            }
-
-            zip = newZip;
-            long ticFreq = HOUR / zip;
-            ticFrequency = ticFreq;
-
-            init();
-        } catch (ParseException e) {
-            e.printStackTrace();
         }
+
+        if (properties.get("zip").isString()) {
+            setZip(properties.get("zip").asString());
+        } else if (properties.get("zip").isNumber()) {
+            setZip(properties.get("zip").asDouble() + "");
+        }
+
+        setTicFrequency();
+        init();
 
         return getProperties();
     }
 
-    public final Object getProperties() {
+    /**
+     * Set the start time to a specific time, current time
+     * or 0 otherwise (parsing error with log).
+     *
+     * @param start local date/time as:
+     *              'yyyy-MM-dd HH:mm:ss' to set a specific date
+     *              'actual' to use the JVM time
+     */
+    private void setStartTime(final String start) {
+        if (start.compareTo("actual") == 0) {
+            startTS = System.currentTimeMillis();
+        } else {
+            try {
+                long localStart = df.parse(start).getTime();
+                startTS = localStart - timezone.getOffset(localStart);
+            } catch (ParseException e) {
+                startTS = 0;
+                logError("Could not parse start parameter '"
+                        + start + "', Set 0 instead.");
+            }
+        }
+    }
+
+    /**
+     * Set the time compression factor. The String can be a number
+     * alone or starting with 'x'.
+     *
+     * @param aZipFactor the time compression factor
+     */
+    private void setZip(final String aZipFactor) {
+        try {
+            zip = Integer.valueOf(aZipFactor.replace("x", ""));
+            if (zip == 0) {
+                zip = 1;
+            }
+        } catch (NumberFormatException e) {
+            zip = 1;
+        }
+    }
+
+    /**
+     * Set the tic frequency to one tic per hour.
+     * (in simulation time)
+     */
+    private void setTicFrequency() {
+        ticFrequency = HOUR / zip;
+    }
+
+    /**
+     * @return return a Json with the start date, zip and status.
+     */
+    public final JsonObject getProperties() {
         JsonObject prop = new JsonObject();
         prop.add("startDate", startTS);
         prop.add("zip", zip);
@@ -314,16 +460,23 @@ public class Timekeeper extends Service implements ModelListener {
         return prop;
     }
 
+    /**
+     * When stopping the component, stop the scheduled thread.
+     */
     @Stop
     public final void stop() {
-        stpe.shutdownNow();
+        if (stpe != null) {
+            stpe.shutdownNow();
+        }
     }
 
     @Override
-    public void modelUpdated() {
+    public final void modelUpdated() {
         if (isFirstModelUpdate()) {
-            sendRequest(new Request(getFullId(), getNode() + ".http", getCurrentTime(),
-                            "addHandler", new Object[]{"/timekeeper", getFullId(), true}),
+            sendRequest(new Request(getFullId(),
+                            getNode() + ".http", getCurrentTime(),
+                            "addHandler",
+                            new Object[]{"/timekeeper", getFullId(), true}),
                     new ShowIfErrorCallback());
         }
         if (status == null || status.equals(Status.UNKNOWN)) {
@@ -335,6 +488,9 @@ public class Timekeeper extends Service implements ModelListener {
         super.modelUpdated();
     }
 
+    /**
+     * Create a dedicated thread for scheduled events.
+     */
     private void initExecutor() {
         stpe = new ScheduledThreadPoolExecutor(1, r -> {
             return new Thread(r, getFullId() + "-timekeeper-pool");
--- src/main/java/org/activehome/timekeeper/TimekeeperRequestHandler.java
+++ src/main/java/org/activehome/timekeeper/TimekeeperRequestHandler.java
@@ -2,7 +2,7 @@ package org.activehome.timekeeper;
 
 /*
  * #%L
- * Active Home :: :: Timekeeper
+ * Active Home :: Timekeeper
  * $Id:$
  * $HeadURL:$
  * %%
@@ -27,46 +27,95 @@ package org.activehome.timekeeper;
 
 import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
-import org.activehome.com.Request;
 import org.activehome.service.RequestHandler;
 import org.activehome.tools.file.FileHelper;
 import org.activehome.tools.file.TypeMime;
 
 /**
+ * Methods of the timekeeper accessible from other
+ * components through {@code Service#getRequest}.
+ *
  * @author Jacky Bourgeois
- * @version %I%, %G%
  */
 public class TimekeeperRequestHandler implements RequestHandler {
 
-    private final Request request;
+    /**
+     * The service to access.
+     */
     private final Timekeeper service;
 
-    public TimekeeperRequestHandler(Request request, Timekeeper service) {
-        this.request = request;
-        this.service = service;
+    /**
+     * Constructor, restricted to package access, to be
+     * called by {@code Timekeeper} only.
+     * @param timekeeperService the {@code Timekeeper} instantiating the handle
+     */
+    TimekeeperRequestHandler(final Timekeeper timekeeperService) {
+        service = timekeeperService;
     }
 
-    public long getTime() {
+    /**
+     * Get the current UTC time: emulated idle/running or actual.
+     *
+     * @return UNIX timestamp.
+     */
+    public final long getTime() {
         return service.getUTCTime();
     }
 
-    public boolean startTime() {
+    /**
+     * Switch the status to INITIALIZED and send a Tic
+     * which contains the time command INIT.
+     *
+     * @return true
+     */
+    public boolean initTime() {
+        return service.init();
+    }
+
+    /**
+     * Start sending Tic at the given time zip and frequency.
+     * The first Tic contains the time command START.
+     *
+     * @return true if the time was initialize before
+     */
+    public final boolean startTime() {
         return service.startTime();
     }
 
-    public boolean stopTime() {
+    /**
+     * Stop sending regular Tics with a last one containing
+     * the time command STOP.
+     *
+     * @return true if time was running or idled
+     */
+    public final boolean stopTime() {
         return service.stopTime();
     }
 
-    public boolean pauseTime() {
+    /**
+     * Stop sending regular Tics with a last one containing
+     * the time command PAUSE.
+     *
+     * @return true if the time was running
+     */
+    public final boolean pauseTime() {
         return service.pauseTime();
     }
 
-    public boolean resumeTime() {
+    /**
+     * Restart sending regular Tics with the first one containing
+     * the time command RESUME.
+     *
+     * @return true if the time was idled
+     */
+    public final boolean resumeTime() {
         return service.resumeTime();
     }
 
-    public JsonValue html() {
+    /**
+     * @return an String containing a html view of the timekeeper
+     */
+    public final JsonValue html() {
         JsonObject wrap = new JsonObject();
         wrap.add("name", "timekeeper-view");
         wrap.add("url", service.getId() + "/timekeeper-view.html");
@@ -78,26 +127,46 @@ public class TimekeeperRequestHandler implements RequestHandler {
         return json;
     }
 
-    public JsonValue file(String str) {
-        String content = FileHelper.fileToString(str, getClass().getClassLoader());
-        if (str.compareTo("timekeeper-view.html") == 0) content = content.replaceAll("\\$\\{id\\}", service.getId());
+    /**
+     * Serve content file as string.
+     *
+     * @param fileName the file to serve
+     * @return a json containing the file as string
+     * in 'content' and the type of the file in 'mime'
+     */
+    public final JsonValue file(final String fileName) {
+        String content = FileHelper.fileToString(
+                fileName, getClass().getClassLoader());
+        if (fileName.compareTo("timekeeper-view.html") == 0) {
+            content = content.replaceAll("\\$\\{id\\}", service.getId());
+        }
         JsonObject json = new JsonObject();
         json.add("content", content);
-        json.add("mime", TypeMime.valueOf(str.substring(str.lastIndexOf(".") + 1, str.length())).getDesc());
+        json.add("mime", TypeMime.valueOf(
+                fileName.substring(fileName.lastIndexOf(".") + 1,
+                        fileName.length())).getDesc());
         return json;
     }
 
-    public Object setProperties(JsonObject properties) {
+    /**
+     * Set the timekeeper properties.
+     * Properties can be
+     * - timezone (e.g 'Europe/Amsterdam' or 'UTC')
+     * - start (e.g 'yyyy-MM-dd HH:mm:ss', 'actual' or UNIX timestamp)
+     * - zip (e.g 1 or 'x1')
+     *
+     * @param properties properties as Json
+     * @return {@code #getProperties} showing the new status of all properties.
+     */
+    public final JsonObject setProperties(final JsonObject properties) {
         return service.setProperties(properties);
     }
 
-    public Object getProperties() {
+    /**
+     * @return return a Json with the start date, zip and status.
+     */
+    public final JsonObject getProperties() {
         return service.getProperties();
     }
-
-    public boolean initTime() {
-        return service.init();
-    }
-
 
 }
